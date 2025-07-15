df26fd35cb19938512886d12c76e49183b2e5fa7
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-07-15 22:29:26 +0300

    newcommit

---
+++ src/test/java/ru/mihail_tarasov/licensing_service/LicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mihail_tarasov.licensing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
