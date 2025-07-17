ab1071cc1419e494ee61e575e35ff6b7919ff42f
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-07-17 22:13:08 +0300

    добавил методы для PUT, POST, DELETE

--- src/main/java/ru/mihail_tarasov/licensing_service/controller/LicenseController.java
+++ src/main/java/ru/mihail_tarasov/licensing_service/controller/LicenseController.java
@@ -1,9 +1,35 @@
 package ru.mihail_tarasov.licensing_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.mihail_tarasov.licensing_service.model.License;
+import ru.mihail_tarasov.licensing_service.service.LicenseService;
 
 @RestController
 @RequestMapping(value="v1/organization/{organizationId}/license")
         public class LicenseController {
+        @Autowired
+        private LicenseService licenseService;
+
+        @GetMapping(value = "/{licenseId")
+        public ResponseEntity<License>getLicense(
+                @PathVariable("organizationId") String organizationId,
+                @PathVariable("licenseId") String licenseId){
+
+                License license= licenseService.getLicense(licenseId,organizationId);
+                return ResponseEntity.ok(license);
+        }
+
+        @PutMapping
+        public ResponseEntity<String>updateLicense(
+                @PathVariable("organizationId") String organizationId,@RequestBody License request){
+                return ResponseEntity.ok(licenseService.createlicense(request,organizationId));
+        }
+
+        @DeleteMapping(value = "/{licenseId")
+        public ResponseEntity<String> deleteLicense(
+                @PathVariable("organizationId") String organizationId,@PathVariable("licenseId") String licenseId) {
+                return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+        }
 }
